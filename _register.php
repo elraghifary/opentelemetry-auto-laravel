2edef83eee8490c6bfcd0f0ee82d1eef5358f676
elraghifary <elraghifary.dev@gmail.com>
2023-11-09 12:29:04 +0700

    initial laravel auto instrumentation

--- _register.php
+++ _register.php
@@ -11,8 +11,6 @@ if (class_exists(Sdk::class) && Sdk::isInstrumentationDisabled(LaravelInstrument
 
 if (extension_loaded('opentelemetry') === false) {
     trigger_error('The opentelemetry extension must be loaded in order to autoload the OpenTelemetry Laravel auto-instrumentation', E_USER_WARNING);
-
-    return;
 }
 
 LaravelInstrumentation::register();
